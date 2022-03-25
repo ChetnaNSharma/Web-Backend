89756256296e63392d64ddb46b380021dcc167ae
ChetnaNSharma <chetna.ns.sharma@gmail.com>
2022-03-25 23:34:50 +0530

    continuing validations

--- Products - Schema/Product.js
+++ Products - Schema/Product.js
@@ -10,16 +10,35 @@ mongoose.connect('mongodb://localhost:27017/myShop',{useNewUrlParser: true}
 
 const productSchema = new mongoose.Schema({
     name: {
-        type: String
-        required: true
+        type: String,
+        required: true,
+        maxlength: 20
     },
     price: {
-        type: Number
+        type: Number,
+        required: true,
+        min: 0
     },
+    onSale:{
+        type: Boolean,
+        default: false
+    },
+    categories: [String],
+    qty: {
+        online:{
+            type: Number,
+            default: 0
+        },
+        inStore:{
+            type: Number,
+            default: 0
+        }
+    }
+    }
 })
 
 const Product = mongoose.model('Product','productSchema')
-const bike = new Product({name: 'Mountain Bike', price: 599, color: 'red'})
+const bike = new Product({name: 'Mountain Bike', price: 599, color: 'red', categories: ['cycling', 'safety']})
 bike.save()
 .then( data => {
     console.log(data);
